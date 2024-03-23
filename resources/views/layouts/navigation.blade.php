bcab6db10f8fd74809c5d3314e13bc5839b6268f
juki3r <deepong25@gmail.com>
2024-03-23 19:55:18 +0800

    modified:   resources/views/layouts/navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <img src="ajclogo.png" alt="logo">
+                        <img src="ajclogo.png" alt="logo" width="330px">
                     </a>
                 </div>
 
