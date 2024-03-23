8700d927c25522a6b41bc356a90ec76076faa68f
juki3r <deepong25@gmail.com>
2024-03-23 22:20:46 +0800

    modified:   resources/views/layouts/app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@
 
         <title>{{ config('app.name', 'AJC PISONET') }}</title>
 
+        <link rel="shortcut icon" href="ajclogo.png" type="image/x-icon">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
