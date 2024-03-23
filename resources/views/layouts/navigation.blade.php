41d9e1e106fae84890ce3d748449abb4ef5d00c5
juki3r <deepong25@gmail.com>
2024-03-23 20:27:06 +0800

    modified:   resources/views/components/nav-link.blade.php 	modified:   resources/views/layouts/navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,7 +12,7 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                        <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" class="text-light">
+                        <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                             {{ __('Dashboard') }}
                         </x-nav-link>
                     @if(Auth::user()->usertype == 'admin')
