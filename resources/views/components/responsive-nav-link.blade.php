c71fa2d242e8de97ba30353cc02267a7442a7d41
juki3r <deepong25@gmail.com>
2024-03-23 20:35:55 +0800

    modified:   resources/views/components/responsive-nav-link.blade.php

--- resources/views/components/responsive-nav-link.blade.php
+++ resources/views/components/responsive-nav-link.blade.php
@@ -2,7 +2,7 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'block w-full pl-3 pr-4 py-2 border-l-4 border-white text-left text-base font-medium text-white bg-white focus:outline-none focus:text-white focus:bg-white focus:border-white transition duration-150 ease-in-out'
+            ? 'block w-full pl-3 pr-4 py-2 border-l-4 border-white text-left text-base font-medium text-dark bg-white focus:outline-none focus:text-dark focus:bg-white focus:border-white transition duration-150 ease-in-out'
             : 'block w-full pl-3 pr-4 py-2 border-l-4 border-transparent text-left text-base font-medium text-white hover:text-white hover:bg-white hover:border-white focus:outline-none focus:text-white focus:bg-white focus:border-white transition duration-150 ease-in-out';
 @endphp
 
