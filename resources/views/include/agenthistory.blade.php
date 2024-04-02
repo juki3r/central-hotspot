eee13017a7223a0bcdccf2678ed636f9f98c7bab
juki3r <deepong25@gmail.com>
2024-04-02 08:59:29 +0800

    modified:   resources/views/include/agenthistory.blade.php

--- resources/views/include/agenthistory.blade.php
+++ resources/views/include/agenthistory.blade.php
@@ -14,19 +14,24 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                   <table>
-                        <thead>
-                            <td>Vouchers</td>
-                        </thead>
-                        <tbody>
-                            @foreach ($history as $history)
-                            <tr>
-                                <td>{{$history->voucher}}</td>
-                            </tr>    
-                            @endforeach
-                            
-                        </tbody>
-                   </table>
+                    <div class="table-responsive">
+                        <table class="table table-bordered table-sm text-nowrap p-0 m-0" id="sortTable">
+                            <thead class="fs-5">
+                                <td>Vouchers</td>
+                                <td>Price</td>
+                                <td>Sold at</td>
+                            </thead>
+                            <tbody>
+                                @foreach ($history as $history)
+                                <tr>
+                                    <td>{{$history->voucher}}</td>
+                                    <td>{{$history->price}}</td>
+                                    <td>{{$history->sold_at}}</td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
