8760bd04c169dec2144d4acfd0afa8e93c56302b
juki3r <deepong25@gmail.com>
2024-04-02 13:02:21 +0800

    modified:   app/Http/Controllers/VoucherController.php

--- app/Http/Controllers/VoucherController.php
+++ app/Http/Controllers/VoucherController.php
@@ -98,8 +98,12 @@ class VoucherController extends Controller
             return redirect('dashboard');
         }
         $active_agents = User::where('usertype', 'agent')->get('name');
-        $get_sales = Voucher::where('sold_by', '!=', null)->get('sold_by');
-        return $get_sales;
+        
+        return $active_agents->name;
+
+        // $get_sales = Voucher::where('sold_by', '!=', null)->get('sold_by');
+     
+        
         // return view ('include.adminsales', ['active_agents' => $active_agents, 'get_sales' => $get_sales]);
 
     }
