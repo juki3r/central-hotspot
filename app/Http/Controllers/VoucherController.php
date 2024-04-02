4398dda020e711491973dee86206c0ad29a754ea
juki3r <deepong25@gmail.com>
2024-04-02 12:59:33 +0800

    modified:   app/Http/Controllers/VoucherController.php

--- app/Http/Controllers/VoucherController.php
+++ app/Http/Controllers/VoucherController.php
@@ -99,7 +99,7 @@ class VoucherController extends Controller
         }
         $active_agents = User::where('usertype', 'agent')->get('name');
         $get_sales = Voucher::where('sold_by', '!=', null)->get();
-        return $get_sales->price;
+        return $get_sales;
         // return view ('include.adminsales', ['active_agents' => $active_agents, 'get_sales' => $get_sales]);
 
     }
