58e40590c3e8e771f9496fb50e035f55e4687dfd
juki3r <deepong25@gmail.com>
2024-04-02 08:47:16 +0800

    modified:   resources/views/include/agenthistory.blade.php

--- resources/views/include/agenthistory.blade.php
+++ resources/views/include/agenthistory.blade.php
@@ -8,13 +8,25 @@
 
     {{-- AGENTS --}}
     
-    {{$history}}
+    
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                   
+                   <table>
+                        <thead>
+                            <td>Vouchers</td>
+                        </thead>
+                        <tbody>
+                            @foreach ($history as $history)
+                            <tr>
+                                <td>{{$history->voucher}}</td>
+                            </tr>    
+                            @endforeach
+                            
+                        </tbody>
+                   </table>
                 </div>
             </div>
         </div>
