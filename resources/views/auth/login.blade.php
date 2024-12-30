edec5b2b7e7a4fd3f150b54d7ed93a18cbd7df77
juki3r <deepong25@gmail.com>
2024-12-30 23:28:35 +0800

    modified:   resources/views/auth/login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,8 @@
 <x-guest-layout>
     <!-- Session Status -->
-    <x-auth-session-status class="mb-4 border-rounded d-none" :status="session('status')" />
+    <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
+    <form method="POST" action="{{ route('login') }}" class="bg-info">
         @csrf
 
         <!-- Email Address -->
