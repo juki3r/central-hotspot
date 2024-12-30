bd9aa69523a81ef8c2c64c9fcd5428acd6852852
juki3r <deepong25@gmail.com>
2024-12-30 23:31:04 +0800

    modified:   resources/views/layouts/guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -33,7 +33,7 @@
                 </a>
             </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 bg-gray-100 py-4 shadow-sm overflow-hidden sm:rounded-lg">
+            <div class="w-full sm:max-w-md mt-6 px-6 bg-gray-100 py-4 shadow-sm overflow-hidden sm:rounded-lg sm:rounded">
                 {{ $slot }}
             </div>
         </div>
